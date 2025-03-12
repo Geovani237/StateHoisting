553f0a2482ed11ed424949b6378b4550f48d5194
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2025-03-12 20:58:02 -0300

    Criando um observável e um observador para capital

--- app/src/main/java/br/com/fiap/statehosting/juros/JurosScreenViewModel.kt
+++ app/src/main/java/br/com/fiap/statehosting/juros/JurosScreenViewModel.kt
@@ -1,7 +1,12 @@
 package br.com.fiap.statehosting.juros
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class JurosScreenViewModel: ViewModel() {
-    
+    private val _capital = MutableLiveData<String>()
+    val capital: LiveData<String> = _capital
+
+
 }
